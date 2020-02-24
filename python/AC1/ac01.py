47fae47187c58a912c39c4cb48c36946020effd4
AmandaDesenv <amanda89conceicao@gmail.com>
2020-02-23 22:35:26 -0300

    Término das Funções da AC01 de LP2

--- python/AC1/ac01.py
+++ python/AC1/ac01.py
@@ -3,21 +3,38 @@
 # Aluno 02: Amanda Conceição Gomes
 # Aluno 03: Vinícius Tertuliano da Silva
 
+
 def adicionar_aluno(alunos, nome, notas):
     if nome in alunos:
         return alunos
     else:
-        alunos[nome]=notas
-    return alunos
+        alunos[nome] = notas
+        return alunos
+
 
 def remover_aluno(alunos, nome):
     if nome in alunos:
-        alunos.pop(nome)    
-    return alunos 
+        alunos.pop(nome)
+    return alunos
 
-def pesquisar_aluno(alunos, nome):    
+
+def pesquisar_aluno(alunos, nome):
     if nome in alunos:
-        print ("O nome aluno é: {}".format(nome))
-        print ("As notas do {} são {}: ".format(nome, alunos[nome]))
+        return alunos[nome]
     else:
-        return print ([])
+        return []
+
+
+def calcular_media(alunos, nome):
+    if nome in alunos:
+        return '{:.1f}'.format(sum(alunos[nome])/len(alunos[nome]))
+    return 0
+
+
+def calcular_media_turma(alunos):
+    somaNotas = 0
+    somaTnotas = 0
+    for aluno in alunos:
+        somaNotas += sum(alunos[aluno])
+        somaTnotas += len(alunos[aluno])
+    return '{:.1f}'.format(somaNotas/somaTnotas)
